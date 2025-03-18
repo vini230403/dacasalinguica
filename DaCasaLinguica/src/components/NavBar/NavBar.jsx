f25b725236798489d129aa0f49a29a84a7c137bf
vini230403 <vini2003.melo@gmail.com>
2025-03-18 20:16:32 -0300

    personalizando a navBar

--- DaCasaLinguica/src/components/NavBar/NavBar.jsx
+++ DaCasaLinguica/src/components/NavBar/NavBar.jsx
@@ -5,12 +5,21 @@ import { NavLink } from "react-router-dom"
 const NavBar = () => {
   return (
     <header>
-        <img src="" alt="logo" />
+        <img src="/public/img/logo.svg" alt="logo" className='logo' />
         <h1 className='title'>Da Casa</h1>
-        <nav className='navabar'>
+        <nav className='navbar'>
             <ul className='nav-list'>
                 <li>
-                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/" className="navlink">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/" className="navlink">Produto</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/" className="navlink">Sobre Nós</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/" className="navlink">Contato</NavLink>
                 </li>
             </ul>
         </nav>
