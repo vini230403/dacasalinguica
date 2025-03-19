4fd5dead2b37c76200f37b39d1472ba1387e0cd5
vini230403 <vini2003.melo@gmail.com>
2025-03-19 12:06:20 -0300

    estilizando navBar adicionando slide e conteudo na home ajustando navBar para mobile

--- DaCasaLinguica/src/components/NavBar/NavBar.jsx
+++ DaCasaLinguica/src/components/NavBar/NavBar.jsx
@@ -1,14 +1,31 @@
 import './NavBar.css'
 
+import { useState } from 'react'
+
 import { NavLink } from "react-router-dom"
 
+
 const NavBar = () => {
+
+    const [isModalMenu, setIsModalMenu] = useState(false)
+
+    const toggleModal = () => {
+        setIsModalMenu(!isModalMenu)
+    }
+
+
+
   return (
     <header>
         <img src="/public/img/logo.svg" alt="logo" className='logo' />
-        <h1 className='title'>Da Casa</h1>
+        <h1 className='title'>Linguiças Artesanais</h1>
         <nav className='navbar'>
-            <ul className='nav-list'>
+            <div class="menu-icon" id="menuIcon" onClick={toggleModal}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul className={`nav-list ${isModalMenu ? 'open' : ''}`} id="navList">
                 <li>
                     <NavLink to="/" className="navlink">Home</NavLink>
                 </li>
@@ -22,6 +39,27 @@ const NavBar = () => {
                     <NavLink to="/contato" className="navlink">Contato</NavLink>
                 </li>
             </ul>
+            <div className={`modal ${isModalMenu ? 'open' : ''}`} id='modalMenu'>
+                <div className="menu-icon" id="modalMenuIcon" onClick={toggleModal}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <ul className='nav-list'>
+                    <li>
+                        <NavLink to="/" className="navlink" onClick={toggleModal}>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/produtos" className="navlink" onClick={toggleModal}>Produtos</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/sobrenos" className="navlink" onClick={toggleModal}>Sobre Nós</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contato" className="navlink" onClick={toggleModal}>Contato</NavLink>
+                    </li>
+                </ul>
+            </div>
         </nav>
     </header>
   )
--- DaCasaLinguica/src/pages/home/Home.jsx
+++ DaCasaLinguica/src/pages/home/Home.jsx
@@ -1,9 +1,38 @@
 import './Home.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
 const Home = () => {
   return (
-    <div>
-        <h1>Home</h1>
+    <div className="slider-container">
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        spaceBetween={10}
+        slidesPerView={1}
+        navigation={{
+          nextEl: '.custom-next',
+          prevEl: '.custom-prev'
+        }}
+        pagination={{ el: '.custom-pagination', clickable: true}}
+        autoplay={{ delay: 5000, disableOnInteraction: false}}
+        loop={true}
+      >
+        <SwiperSlide><img src="/public/img/banner_dacasa.jpg" alt="banner" /></SwiperSlide>
+        <SwiperSlide><img src="/public/img/cortada.jpg" alt="cortada" /></SwiperSlide>
+      </Swiper>
+      <div className="controls-container">
+        <div className="custom-prev"><FaChevronLeft /></div>
+        <div className="custom-pagination"></div>
+        <div className="custom-next"><FaChevronRight /></div>
+      </div>
+      <div className='texto'>
+        <h1>Sabor autêntico em cada mordida, feito à mão com paixão!</h1>
+        <img src="/public/img/novidades.png" alt="novidade" className='img-center' />
+      </div>
     </div>
   )
 }
