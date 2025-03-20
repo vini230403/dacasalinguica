c8a6dff97081a4e723ddc5f32dc6fa7489608825
pedroBaptistaDosSantos <pedrobaptista.dev@gmail.com>
2025-03-20 00:28:53 -0300

    Ajuste caminho do componente

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NavBar from './components/NavBar/NavBar'
 import Home from './pages/home/Home';
 import Produtos from './pages/produtos/Produtos';
-import SobreNos from './pages/SobreNos/SobreNos';
+import SobreNos from './pages/sobreNos/SobreNos';
 import Contato from './pages/contato/Contato'
 
 function App() {
